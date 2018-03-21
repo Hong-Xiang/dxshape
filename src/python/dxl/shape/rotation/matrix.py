fix random rotation bugs for axis_to_z and z_to_axis, add axis2axis

src/python/dxl/shape/rotation/matrix.py
@@ -33,10 +33,7 @@ def axis_to_z(axis: Axis3) -> np.ndarray:
     Rotation matrix rotate given axis to normal z axis
     """
     axis_z = axis.direction_vector().z()
-    if axis_z >= 0:
-        rot_y = math.asin(1. - axis_z)
-    else:
-        rot_y = math.asin(- axis_z - 1.) + np.pi
+    rot_y = math.acos(axis_z)
     rot_z = math.atan2(axis.direction_vector().y(),
                        axis.direction_vector().x())
     return rotate3(rot_y, AXIS3_Y)@rotate3(-rot_z, AXIS3_Z)
@@ -47,10 +44,7 @@ def z_to_axis(axis: Axes3):
     rotation matrix which rotate normal z axis to z axis of given axes
     """
     axis_z = axis.direction_vector().z()
-    if axis_z >= 0:
-        rot_y = math.asin(1. - axis_z)
-    else:
-        rot_y = math.asin(- axis_z - 1.) - np.pi
+    rot_y = math.acos(axis_z)
     rot_z = math.atan2(axis.direction_vector().y(),
                        axis.direction_vector().x())
     return rotate3(rot_z, AXIS3_Z)@rotate3(-rot_y, AXIS3_Y)
@@ -62,4 +56,4 @@ def axis_to_axis(source, target):
     Implemented by firstly roteta source axes to `AXES3_STD.z`, and then rotate
     `AXES3_STD.z` to target axis.
     """
-    return axis_to_z(source) @ z_to_axis(target)
+    return z_to_axis(target)@axis_to_z(source)