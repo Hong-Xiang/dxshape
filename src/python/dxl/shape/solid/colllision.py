c004f32b7ade15e72cad6d0084bb18766c4ac68a
lvli <lvli0126@sjtu.edu.cn>
2018-07-14 21:47:18 +0800

    modified colllision

--- src/python/dxl/shape/solid/colllision.py
+++ src/python/dxl/shape/solid/colllision.py
@@ -17,17 +17,23 @@ class CollisionTester:
         pass
 
 
-class BoxPointTester:
-    def is_collision( p: Point, b: Box) -> bool:
-        if isinstance(p, Box):
-            p, b = b, p
-        boolValue=False
-        bTrans=b.translate(-b.origin)
-        pTtans=p.translate(-b.origin)
-        RotateMatrix=axis_to_axis(b.normal,Vector3([0.0, 0.0, 1.0]))
-        pRot=np.dot(RotateMatrix, pTtans)
-        if ((pRot[1]<=0.5*bTrans. shape[1] or pRot[1]>=-0.5*bTrans. shape[1]) and 
+#class BoxPointTester:
+#    def __init__(self, p: Point, b: Box):
+#        if isinstance(p, Box):
+#            p, b = b, p
+#        self._point = p
+#        self._box = b
+
+def is_collision(p: Point, b: Box) -> bool:
+    if isinstance(p, Box):
+        p, b = b, p
+    boolValue=False
+    bTrans=.translate(-b.origin)
+    pTtans=p.translate(-b.origin)
+    RotateMatrix=axis_to_axis(b.normal,Vector3([0.0, 0.0, 1.0]))
+    pRot=np.dot(RotateMatrix, pTtans)
+    if ((pRot[1]<=0.5*bTrans. shape[1] or pRot[1]>=-0.5*bTrans. shape[1]) and 
         (pRot[2]<=0.5*bTrans. shape[2] or pRot[2]>=-0.5*bTrans. shape[2]) and
         (pRot[3]<=0.5*bTrans. shape[3] or pRot[3]>=-0.5*bTrans. shape[3])):
-            boolValue=not boolValue
-        return boolValue
+        boolValue=not boolValue
+    return boolValue
