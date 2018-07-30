d84f858dceca1167ab97086a277d268a31684624
HongXiang <hx.hongxiang@gmail.com>
2018-07-30 14:51:05 +0800

    add divide

--- src/python/dxl/shape/function/box.py
+++ src/python/dxl/shape/function/box.py
@@ -4,7 +4,8 @@ from dxl.shape.data import Vector
 
 
 def divide(b: Box, grid: List[int]) -> List[Box]:
-    subbox_prototype == Box(sub_box_shape(b, grid))
+    subbox_prototype = Box(sub_box_shape(b, grid))
+    return moves(offsets(b, grid)).fmap(lambda v: subbox_prototype.translate(v))
 
 
 def sub_box_shape(b: Box, grid: List[int]) -> Vector:
@@ -16,3 +17,8 @@ def offsets(b: Box, grid: List[int]) -> Vector:
     steps = [[s*i - o/2 for i in range(g)]
              for s, g, o in zip(step, grid, b.origin)]
     return steps
+
+
+def moves(steps):
+    xs, ys, zs = steps
+    return List([Vector([x, y, z]) for x in xs for y in ys for z in zs])
