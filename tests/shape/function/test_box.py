11c5bde8f44406de5d3e6893b1e2966dd1e46b05
HongXiang <hx.hongxiang@gmail.com>
2018-07-30 15:45:56 +0800

    add divide result type check

--- tests/shape/function/test_box.py
+++ tests/shape/function/test_box.py
@@ -7,7 +7,8 @@ def test_sub_box_shape():
     assert all_close(sub_box_shape(Box([1.0, 2.0, 3.0]), [10, 40, 30]),
                      Vector([0.1, 0.05, 0.1]))
 
+
 def test_divide():
     result = divide(Box([1.0, 2.0, 3.0]), [10, 20, 30])
+    assert isinstance(result, List)
     assert len(result) == 10 * 20 * 30
-
