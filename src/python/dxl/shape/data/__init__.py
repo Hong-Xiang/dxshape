f7a803b9cda40fc7efec302876d506912e84252c
HongXiang <hx.hongxiang@gmail.com>
2018-09-15 15:19:57 +0800

    add rotation_by_matrix and visual

--- src/python/dxl/shape/data/__init__.py
+++ src/python/dxl/shape/data/__init__.py
@@ -1,6 +1,6 @@
 from .base import Entity
 from .axis import *
-from .point import Point
+from .point import *
 from .line import Line
 from .box import Box
 from .group import *
--- src/python/dxl/shape/data/box.py
+++ src/python/dxl/shape/data/box.py
@@ -5,6 +5,7 @@ import numpy as np
 import math
 import attr
 
+
 @dataclass
 class Box(Entity):
     shape: Vector = attr.ib(converter=Vector)
@@ -33,15 +34,16 @@ class Box(Entity):
 
     def is_collision(self, p: 'Entity') -> bool:
         from dxl.shape.data.axis import Axis
-        from dxl.shape.function.rotation.matrix import axis_to_z 
-        p_tran_rot = p.translate(-self.origin).origin @ transpose(axis_to_z(self.normal))
+        from dxl.shape.function.rotation.matrix import axis_to_z
+        p_tran_rot = p.translate(-self.origin).origin @ transpose(
+            axis_to_z(self.normal))
         if ((-self.shape.x / 2 <= p_tran_rot.x < self.shape.x / 2) and
             (-self.shape.y / 2 <= p_tran_rot.y < self.shape.y / 2) and
-            (-self.shape.z / 2 <= p_tran_rot.z < self.shape.z / 2)):
+                (-self.shape.z / 2 <= p_tran_rot.z < self.shape.z / 2)):
             return True
         else:
             return False
-            
+
     def fmap(self, f):
         return Box(self.shape, f(self.origin), f(self.normal))
 
@@ -49,3 +51,6 @@ class Box(Entity):
         if not isinstance(b, Box):
             return False
         return all_close(self.shape, b.shape) and all_close(self.origin, b.origin) and all_close(self.normal, b.normal)
+
+
+
