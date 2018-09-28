dbe3c64a7fe06ffb18b0a5bff853644110672133
hongjiang <hongjiangdong@outlook.com>
2018-09-28 16:51:42 +0800

    new version update

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 setup(
     name='dxl-shape',
-    version='0.1.0',
+    version='0.1.1',
     description='Shape library.',
-    url='https://github.com/Hong-Xiang/dxshape',
+    url='https://github.com/tech-pi/dxshape',
     author='Hong Xiang',
     author_email='hx.hongxiang@gmail.com',
     license='MIT',
     namespace_packages=['dxl'],
     packages=find_packages('src/python'),
     package_dir={'': 'src/python'},
-    install_requires=[],
+    install_requires=['doufo>=0.0.3'],
     scripts=[],
     #   namespace_packages = ['dxl'],
     zip_safe=False)
