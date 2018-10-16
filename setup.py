9f3f80f3663a1510da51ff36a8a3a30231e3983e
hongjiang <hongjiangdong@outlook.com>
2018-10-16 21:11:50 +0800

    update install requires

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='dxl-shape',
-    version='0.1.1',
+    version='0.1.2',
     description='Shape library.',
     url='https://github.com/tech-pi/dxshape',
     author='Hong Xiang',
@@ -10,7 +10,13 @@ setup(
     namespace_packages=['dxl'],
     packages=find_packages('src/python'),
     package_dir={'': 'src/python'},
-    install_requires=['doufo>=0.0.3'],
+    install_requires=[
+        'doufo==0.0.4',
+        'matplotlib',
+        'typing',
+        'numba',
+        'numpy',
+    ],
     scripts=[],
     #   namespace_packages = ['dxl'],
     zip_safe=False)
